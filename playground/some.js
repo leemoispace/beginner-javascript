279ef219bbe4161a619da077b1933066a842d7da
leemoispace <>
2022-06-02 15:28:21 +0400

    Ep8, numbers

--- playground/some.js
+++ playground/some.js
@@ -1,1 +1,2 @@
 console.log('i am in another file.');
+
--- playground/types.js
+++ playground/types.js
@@ -1,24 +1,37 @@
-console.log('it works');
-const name1 = 'wes';
-const middle = 'leroy';
-const sentence = 'she\'s so "cool"\\';
-const sentence2 = `she\'s so \"cool\"\\`;
-// back tick
+// console.log('it works');
+// const name1 = 'wes';
+// const middle = 'leroy';
+// const sentence = 'she\'s so "cool"\\';
+// const sentence2 = `she\'s so \"cool\"\\`;
+// // back tick
 
-const song = 'a\
-b\
-\
-b\
-';
+// const song = 'a\
+// b\
+// \
+// b\
+// ';
 
-const hello = `hello my name is ${name1},nice to meet u`;
+// const hello = `hello my name is ${name1},nice to meet u`;
 
-const hello2 = `hello my name is ${name1},nice to meet u`;
+// const hello2 = `hello my name is ${name1},nice to meet u`;
 
-const html = `
-    <div>
-    <h2>${name1}</h2>
-    </div>
-`;
+// const html = `
+//     <div>
+//     <h2>${name1}</h2>
+//     </div>
+// `;
 
-document.body.innerHTML = html;
+// document.body.innerHTML = html;
+
+// numbers
+
+const age = 100;
+const name1 = 'leroy';
+
+// Math.floor
+
+// mod
+
+const leftover = 20 % 6;
+
+// https://0.30000000000000004.com/ always use whole numbers.
