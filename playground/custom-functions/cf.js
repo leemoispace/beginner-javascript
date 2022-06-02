68f24a2cb7a7bca577024999b69048987d4862d5
leemoispace <>
2022-06-02 16:10:11 +0400

    Ep13, custom function

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,11 @@
+function calculatebill() {
+  // console.log('running calculate bill!');
+  const total = 100 * 1.25;
+  // console.log(total);
+  return total;
+}
+
+const myTotal = calculatebill();
+// console.log(myTotal);
+
+console.log(`your total bill is ${calculatebill()}`);
