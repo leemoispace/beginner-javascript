bb619194526e18cbc806a635353e77d6fed67fdf
leemoispace <>
2022-06-03 15:59:24 +0400

    Ep23, by adding and remove class, u can control many elements style and attributes.

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,49 +1,64 @@
-// function init() {
-//   const para = document.querySelector('p');
-//   console.log(para);
-// }
+// // function init() {
+// //   const para = document.querySelector('p');
+// //   console.log(para);
+// // }
 
-// document.addEventListener('DOMContentLoaded', init); // wait for loading
+// // document.addEventListener('DOMContentLoaded', init); // wait for loading
 
-// const divs = document.querySelectorAll('div');
-// console.log(divs);
+// // const divs = document.querySelectorAll('div');
+// // console.log(divs);
 
-// const classitems = document.querySelectorAll('.item');
-// console.log(classitems);
+// // const classitems = document.querySelectorAll('.item');
+// // console.log(classitems);
 
-// const imgs = document.querySelectorAll('div img');
+// // const imgs = document.querySelectorAll('div img');
 
-// console.log(imgs);
+// // console.log(imgs);
 
+// // const item2 = document.querySelector('.item2');
+// // const imageinItems = item2.querySelector('img');
+// // console.log(imageinItems);
+// // // narrow down the selecting
+
+// // getElementByxxxxx
+
+// // Ep22 Element Properties and Methods
+
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
 // const item2 = document.querySelector('.item2');
-// const imageinItems = item2.querySelector('img');
-// console.log(imageinItems);
-// // narrow down the selecting
+// const item2Image = document.querySelector('img');
+// const heading = item2.querySelector('h2');
+// // console.dir(heading);
+// // console.dir(heading.textContent); // aware of css styling
+// // console.log(heading.innerText);
 
-// getElementByxxxxx
+// // set property on the element
+// // heading.textContent = 'new content';
+// // console.dir(heading.textContent);
 
-// Ep22 Element Properties and Methods
+// // console.log(heading.innerHTML);
+// // console.log(heading.outerHTML); // including outer tags
 
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2Image = document.querySelector('img');
-const heading = item2.querySelector('h2');
-// console.dir(heading);
-// console.dir(heading.textContent); // aware of css styling
-// console.log(heading.innerText);
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`;
 
-// set property on the element
-// heading.textContent = 'new content';
-// console.dir(heading.textContent);
+// // node and tag
+// // pizzaList.insertAdjacentText('beforebegin', '🍕');
 
-// console.log(heading.innerHTML);
-// console.log(heading.outerHTML); // including outer tags
+// Ep23 classes!
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.add('round');
+pic.classList.toggle('round');
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+console.log(pic.classList);
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+pic.addEventListener('click', toggleRound);
+// by adding and remove class, u can control many elements style and attributes.
+// pic.className
 
-// node and tag
-// pizzaList.insertAdjacentText('beforebegin', '🍕');
-
+//
