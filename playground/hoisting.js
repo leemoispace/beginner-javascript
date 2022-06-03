eb8cb2fdf4b27feedb87e741b136757323be7011
leemoispace <>
2022-06-03 14:23:05 +0400

    Ep18, hoisting, compiler will handle the fucntion declarations first.

---
+++ playground/hoisting.js
@@ -0,0 +1,9 @@
+// compiler will handle the fucntion declarations first. vars not hoisted!
+
+// what does this file do?
+sayHi();
+
+// how does this file do it?
+function sayHi() {
+  console.log('hi');
+}
