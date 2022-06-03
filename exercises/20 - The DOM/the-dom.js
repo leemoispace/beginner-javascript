3241a14b62e825a4838e0c603be2f7d3d43e1abe
leemoispace <>
2022-06-03 15:18:41 +0400

    Ep 21, Select elements in DOM

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,23 @@
+// function init() {
+//   const para = document.querySelector('p');
+//   console.log(para);
+// }
+
+// document.addEventListener('DOMContentLoaded', init); // wait for loading
+
+// const divs = document.querySelectorAll('div');
+// console.log(divs);
+
+// const classitems = document.querySelectorAll('.item');
+// console.log(classitems);
+
+// const imgs = document.querySelectorAll('div img');
+
+// console.log(imgs);
+
+// const item2 = document.querySelector('.item2');
+// const imageinItems = item2.querySelector('img');
+// console.log(imageinItems);
+// // narrow down the selecting
+
+// getElementByxxxxx
