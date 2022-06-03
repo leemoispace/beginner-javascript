23843587e0ed98c02f49f80d223a6494edce6dee
leemoispace <>
2022-06-03 15:42:55 +0400

    Ep22, Element Properties and Methods

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -21,3 +21,29 @@
 // // narrow down the selecting
 
 // getElementByxxxxx
+
+// Ep22 Element Properties and Methods
+
+const p = document.querySelector('p');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = document.querySelector('img');
+const heading = item2.querySelector('h2');
+// console.dir(heading);
+// console.dir(heading.textContent); // aware of css styling
+// console.log(heading.innerText);
+
+// set property on the element
+// heading.textContent = 'new content';
+// console.dir(heading.textContent);
+
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML); // including outer tags
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+
+// node and tag
+// pizzaList.insertAdjacentText('beforebegin', '🍕');
+
